0e5a1d743ac818e2be638b05ebf9888d8feed664
Smoggy <avatar145@ukr.net>
2014-04-29 14:15:06 +0300

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  controller :users do
+    get 'api/index' => 'users#index'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
